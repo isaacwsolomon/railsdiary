4e75def7346e7740309fec98d005d24040486619
Isaac Solomon <isaacwsolomon@gmail.com>
2022-07-09 14:16:35 +0100

    Redirect to entries page on successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user.present? && user.authenticate(params[:password])
        session[:user_id] = user.id
-       redirect_to root_path
+       redirect_to entries_url
     else
         render :new
     end
